92a7dddf2617381ef6bd9d8032bedea868ea026f
Duncan Gough <duncan@suttree.com>
2016-07-03 18:25:38 +0100

    fixing route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  match '/(:page)', :to => 'home#index', :via => :get
   match '/shows/:slug', :to => 'shows#show', :via => :get
   match '/add', :to => 'home#add', :via => :get
   match '/save' ,:to => 'home#save', :via => :post
   match "/delayed_job" => DelayedJobWeb, :anchor => false, via: [:get, :post]
+
+  match '/(:page)', :to => 'home#index', :via => :get
 end
