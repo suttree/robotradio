52a45ebd36493a7f6a77a38748c475097e7cea78
Duncan Gough <duncan@suttree.com>
2016-07-07 20:43:32 +0100

    introduce some randomness

--- app/workers/amy_worker.rb
+++ app/workers/amy_worker.rb
@@ -3,9 +3,11 @@ require 'open-uri'
 
 class AmyWorker
   def self.create
-    doc = Nokogiri::HTML(open('http://amyhref.com'))
-    links = doc.css('.links a')
-    link = links[rand(links.length)].attributes['href'].to_s
-    StoryWorker.delay.create(link)
+    if (rand(5) > 2 )
+      doc = Nokogiri::HTML(open('http://amyhref.com'))
+      links = doc.css('.links a')
+      link = links[rand(links.length)].attributes['href'].to_s
+      StoryWorker.delay.create(link) if link
+    end
   end
 end
