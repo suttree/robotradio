1f0e609a47c56cea3da55c6828c8a2dc7d41b2fa
suttree <duncan@suttree.com>
2016-07-10 11:47:46 +0100

    wip fixing next track

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,11 +20,11 @@
 
 $(function() { 
   // Setup the player to autoplay the next track
-  // mostly taken from view-source:http://kolber.github.io/audiojs/demos/test6.html
+  // mostly taken from http://kolber.github.io/audiojs/demos/test6.html
   var a = audiojs.createAll({
     trackEnded: function() {
-      var next = $('.links .link a.playing').parent().parent().next();
-      if (!next.length) next = $('.links .link a').first();
+      var next = $('.links .link .playing').closest('.link').next();
+      if (!next.length) next = $('.links .link').first();
       next.addClass('playing').siblings().removeClass('playing');
       audio.load($('a.track', next).attr('data-src'));
       title = $('a.track', next).attr('data-title');
@@ -36,13 +36,12 @@ $(function() {
 
   first = $('.links .link a.track').attr('data-src');
   title = $('.links .link a.track').attr('data-title');
-  $('.links .link a').first().addClass('playing');
   audio.load(first);
   $('h3.meta.showtitle').text(title);
 
   $('.links .link a.track').click(function(e) {
     e.preventDefault();
-    $(this).addClass('playing').siblings().removeClass('playing');
+    $(this).parent().parent().addClass('playing').siblings().removeClass('playing');
 
     file = $(this).attr('data-src');
     title = $(this).attr('data-title');
