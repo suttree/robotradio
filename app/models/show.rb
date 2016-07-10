dd52e024e73639c905243e2f69925f890a0d4139
suttree <duncan@suttree.com>
2016-07-10 11:31:53 +0100

    images + paperclip

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,10 @@
 class Show < ActiveRecord::Base
-  has_attached_file :cover_image, styles: { large: '600x300>', medium: '300x300>', thumb: '100x100>' }, default_url: "/images/:style/missing.png"
+  has_attached_file :cover_image, styles: {
+    large: '600x300>',
+    medium: '300x300>',
+    thumb: '100x100#'
+  }, default_url: "/images/:style/missing.png"
+
   validates_attachment_content_type :cover_image, content_type: /\Aimage\/.*\Z/
   validates :slug, uniqueness: true
 end
