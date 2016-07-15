89f1629e8ce12fcf6d692f01d4ea69d484bce67d
suttree <duncan@suttree.com>
2016-07-15 20:15:57 +0100

    longreads worker

---
+++ app/workers/long_reads_worker.rb
@@ -0,0 +1,18 @@
+require 'nokogiri'
+require 'open-uri'
+
+class LongReadsWorker
+  def self.create
+    puts "Searching with longreads 1"
+    doc = Nokogiri::HTML(open('http://www.theatlantic.com/category/longreads/'))
+    links = doc.css('div.body a[data-omni-click]')
+    link = links[rand(links.length)].attributes['href'].to_s
+    StoryWorker.delay.create(link) if link
+
+    puts "Searching with longreads 2"
+    doc = Nokogiri::HTML(open('http://www.newstatesman.com/long-reads'))
+    links = doc.css('ul.article-list h2 a')
+    link = links[rand(links.length)].attributes['href'].to_s
+    StoryWorker.delay.create(link) if link
+  end
+end
