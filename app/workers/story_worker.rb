7c10cbb03399830a175d59c4ac71134c561f09ce
Duncan Gough <duncan@suttree.com>
2016-07-24 18:29:00 +0100

    bigger pieces

--- app/workers/story_worker.rb
+++ app/workers/story_worker.rb
@@ -62,7 +62,7 @@ class StoryWorker
 
     content.scan(/[^\.!?]+[\.!?]/).map(&:strip).each do |sentence|
       piece += sentence + ' '
-      if piece.length > 5000
+      if piece.length > 7500
         pieces << piece
         piece = ''
       end
