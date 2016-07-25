3b6a80b07ef5ff255b318015a8672c9a7d027c04
Duncan Gough <duncan@suttree.com>
2016-07-25 20:38:50 +0100

    not ideal but....

--- app/workers/story_worker.rb
+++ app/workers/story_worker.rb
@@ -85,6 +85,7 @@ class StoryWorker
     file_list.collect{ |f| f.prepend(Rails.root.to_s + '/') }
     `mp3wrap #{Rails.root}/public/content/#{normalised_title}.mp3 #{file_list.join(' ')}`
     `mv #{Rails.root}/public/content/#{normalised_title}_MP3WRAP.mp3 #{Rails.root}/public/content/#{normalised_title}.mp3`
+    `chmod 777 #{Rails.root}/public/content/#{normalised_title}.mp3`
 
     # add some metadata
     image = image.split(' ').first rescue nil
