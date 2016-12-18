4bdcd42bf0751f11cdcb71b596c28dcbac51b313
Duncan Gough <duncan@suttree.com>
2016-12-18 14:10:48 +0000

    adding a new long reads source

--- app/workers/long_reads_worker.rb
+++ app/workers/long_reads_worker.rb
@@ -4,26 +4,36 @@ require 'open_uri_redirections'
 
 class LongReadsWorker
   def self.create
-    puts "Searching with longreads 1"
-    doc = Nokogiri::HTML(open('http://www.theatlantic.com/category/longreads/', :allow_redirections => :safe))
-    links = doc.css('div.body a[data-omni-click]')
-    link = links[rand(links.length)].attributes['href'].to_s
+    probability = 3
+    if (rand(4) > probability)
+      puts "Searching with longreads 1"
+      doc = Nokogiri::HTML(open('http://www.theatlantic.com/category/longreads/', :allow_redirections => :safe))
+      links = doc.css('div.body a[data-omni-click]')
+      link = links[rand(links.length)].attributes['href'].to_s
 
-    if link.present?
-      link = 'http://www.theatlantic.com' + link
-      StoryWorker.delay.create(link)
-    end
+      if link.present?
+        link = 'http://www.theatlantic.com' + link
+        StoryWorker.create(link)
+      end
+    elsif (rand(8) > probability)
+      puts "Searching with longreads 2"
+      doc = Nokogiri::HTML(open('https://longreads.com/articles/search/', :allow_redirections => :safe))
+      links = doc.css('a.h-one.article-title')
+      link = links[rand(links.length)].attributes['href'].to_s
 
-    puts "Searching with longreads 2"
-    doc = Nokogiri::HTML(open('http://www.newstatesman.com/long-reads', :allow_redirections => :safe))
-    links = doc.css('ul.article-list h2 a')
-    link = links[rand(links.length)].attributes['href'].to_s
+      if link.present?
+        StoryWorker.create(link)
+      end
+    else
+      puts "Searching with longreads 3"
+      doc = Nokogiri::HTML(open('http://www.newstatesman.com/long-reads', :allow_redirections => :safe))
+      links = doc.css('ul.article-list h2 a')
+      link = links[rand(links.length)].attributes['href'].to_s
 
-    if link.present?
-      link = 'http://www.newstatesman.com' + link
-puts link.inspect
-puts "LR LINK ---------"
-      StoryWorker.create(link)
+      if link.present?
+        link = 'http://www.newstatesman.com' + link
+        StoryWorker.create(link)
+      end
     end
   end
 end
